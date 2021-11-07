fd3716c986c4eff3667e78e8b9ca144c1acd37a9
Steven Sorokin <67019760+Steven-Sorokin@users.noreply.github.com>
2021-11-07 15:58:34 +0200

    sends reset packet instead of e-land

--- send_emergency_land.py
+++ send_emergency_land.py
@@ -27,19 +27,43 @@ def main(uri):
     link = crtp.get_link_driver(uri)
     assert link is not None
     
-    pk = CRTPPacket(0xFF, [0x03, 0x04, 0,0,0,0,0,0,0])
+    # pk = CRTPPacket(0xFF, [0x03, 0x04, 0,0,0,0,0,0,0])  # packey for e-land
+
+    pk = CRTPPacket(0xf3, [0xfe, 0x02])  #OFF
+    link.send_packet(pk)
+
+    # pk = CRTPPacket(0xf3, [0xfe, 0x03])  #ON
+    # link.send_packet(pk)
+
     ##packet = [0xf3, 0xfe, cmd]
     #class PowerSwitch:
-    # BOOTLOADER_CMD_ALLOFF = 0x01
     # BOOTLOADER_CMD_SYSOFF = 0x02
     # BOOTLOADER_CMD_SYSON = 0x03
-    link.send_packet(pk)
-    print("emergency land command sent")
+
+    print("Restart command sent!\nPower OFF")
     pk_recv = link.receive_packet(0.1)
 
     assert pk_recv is not None
 
     link.close()
+
+    time.sleep(1)
+
+
+    link = crtp.get_link_driver(uri)
+    assert link is not None
+
+    pk = CRTPPacket(0xf3, [0xfe, 0x03])  #ON
+    link.send_packet(pk)
+
+    print("Restart command sent!\nPower ON")
+    pk_recv = link.receive_packet(0.1)
+
+    assert pk_recv is not None
+
+    link.close()
+
+
 
 if __name__ == "__main__":
     main()
